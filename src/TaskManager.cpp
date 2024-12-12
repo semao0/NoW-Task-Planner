da3a590a6c28ecfb7d12e4467c4d53eb406ea120
semao0 <belousovsemen40@gmail.com>
2024-12-12 19:29:18 +0300

    add new in TaskManager.cpp

---
+++ src/TaskManager.cpp
@@ -0,0 +1,24 @@
+#include "TaskManager.h"
+#include <algorithm>
+#include <iostream>
+void TaskManager::addTask(Task* task)
+{
+    tasks.push_back(task);
+}
+void TaskManager::removeTask(Task* task)
+{
+    tasks.erase(std::remove(tasks.begin(), tasks.end(), task), tasks.end());
+}
+void TaskManager::checkDeadlines()
+{
+    for(auto* task : tasks)
+    {
+        if(task->isDeadLineMissed())
+        {
+            std::cout << "Дедлайн задачи \"" << task->getName() << "\" истек!" << std::endl;
+        }
+    }
+}
+const std::vector<Task*>& TaskManager::getTasks() const{
+    return tasks;
+}
